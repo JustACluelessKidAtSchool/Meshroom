f113e9a68985cbd89c3145f2d3a65fed56b01a46
Alice Sonolet <alice.sonolet@gmail.com>
2025-10-24 23:24:54 +0200

    [code] graph : Better management of statuses after task/job actions

--- meshroom/core/taskManager.py
+++ meshroom/core/taskManager.py
@@ -526,7 +526,6 @@ class TaskManager(BaseObject):
             node.destroyed.connect(lambda obj=None, name=node.name: self.onNodeDestroyed(obj, name))
             node.initStatusOnSubmit()
 
-        print("[TaskManager] (submit) graph.updateMonitoredFiles")
         graph.updateMonitoredFiles()
             
         flowEdges = graph.flowEdges(startNodes=toNodes)
